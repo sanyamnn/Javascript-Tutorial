7dac950d638f352c9245a82867b27c6168e075e2
Sanyam <chughsanyam@gmail.com>
2018-01-14 23:38:19 +0530

    added new demo

---
+++ JSdemo0.js
@@ -0,0 +1,18 @@
+var a=2;
+var b=3;
+var c = a+ b; //STATEMENT
+ //OUTPUT METHODS IN JS :
+// 1 ) console.log
+console.log(a); //log-function a - parameter
+//2 - alert
+alert("Welcome to Konfinity");
+
+//INPUT METHODS IN JS :
+//prompt
+var name = prompt("What is your name ?");
+//confirm 
+var work = confirm("Did you complete the work ?");
+console.log("Welcome "+name);
+console.log(work); 
+
+
